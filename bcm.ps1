cf7d499af0f16bfbcdca76caa02a0ec5c81563f6
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2020-01-28 17:20:42 -0400

    Format output of bcm.

--- bcm.ps1
+++ bcm.ps1
@@ -1,1 +1,2 @@
-$args[0] | Measure-Command {./bci $_ | Out-Default}
+$secs = ($args[0] | Measure-Command {./bci $_ | Out-Default}).TotalSeconds
+"The program ran for $([math]::Round($secs, 3)) seconds."
