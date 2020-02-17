ca55e99b7e61f02a7b4f6d3b741ad2a60f239719
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2020-02-17 12:28:50 -0400

    Variable number of samples and print average in sampler script.

--- sample.ps1
+++ sample.ps1
@@ -1,7 +1,8 @@
 $times = @()
-foreach($i in 1..10) {
+foreach($i in 1..$args[1]) {
 	$time = (./bcm $args[0])
 	echo "$($args[0]) ran for $time seconds."
 	$times += $time
 }
 echo $times
+echo "Average is $([math]::Round(($times | Measure-Object -Average).Average, 3))"
