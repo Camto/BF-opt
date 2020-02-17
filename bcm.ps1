a843dafd335aee34db11f556f9cbdebc03e7a9c8
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2020-02-17 13:36:08 -0400

    Sample and bcm with BF program input for GOL.

--- bcm.ps1
+++ bcm.ps1
@@ -1,2 +1,6 @@
-$secs = ($args[0] | Measure-Command {./bci $_ | Out-Default}).TotalSeconds
+if($args.length -lt 2) {
+	$secs = ($args[0] | Measure-Command {./bci $_ | Out-Default}).TotalSeconds
+} else {
+	$secs = (@($args[0],$args[1]),1 | Measure-Command {(./bci $_[0] $_[1]) | Out-Default}).TotalSeconds
+}
 echo $([math]::Round($secs, 5))
--- sample.ps1
+++ sample.ps1
@@ -1,8 +1,22 @@
 $times = @()
-foreach($i in 1..$args[1]) {
-	$time = (./bcm $args[0])
+
+$samples = 0
+if($args.length -lt 3) {
+	$samples = $args[1]
+} else {
+	$samples = $args[2]
+}
+
+foreach($i in 1..$samples) {
+	$time = 0
+	if($args.length -lt 3) {
+		$time = ./bcm $args[0]
+	} else {
+		$time = ./bcm $args[0] $args[1]
+	}
 	echo "$($args[0]) ran for $time seconds."
 	$times += $time
 }
+
 echo $times
 echo "Average is $([math]::Round(($times | Measure-Object -Average).Average, 5))"
