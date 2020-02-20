24472a7c7c9b87234d28d4177c706b1849c82c7e
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2020-02-19 21:37:22 -0400

    Add the "none" optimization.

--- comp.ps1
+++ comp.ps1
@@ -1,7 +1,7 @@
 $program_name = [System.IO.Path]::GetFileNameWithoutExtension($args[0])
 
-$opts = "set", "mult", "linear"
+$opts = "", "set", "mult", "linear"
 foreach($opt in $opts) {
-	"./bfc ""$($args[0])"" ""{opts={$opt=true},outfile='../BF Programs/$program_name$opt.bc'}"""
-	./bfc $args[0] "{opts={$opt=true},outfile='../BF Programs/$program_name$opt.bc'}"
+	"./bfc ""$($args[0])"" ""{opts={['$opt']=true},outfile='../BF Programs/$program_name$opt.bc'}"""
+	./bfc $args[0] "{opts={['$opt']=true},outfile='../BF Programs/$program_name$opt.bc'}"
 }
